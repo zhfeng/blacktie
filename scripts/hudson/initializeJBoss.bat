6516f4cf2e5bac321b9e9565711b0270a2b47bd4
Tom Jenkinson <tom.jenkinson@redhat.com>
2012-03-05 16:04:40 +0000

    Updated to use a common unzip script

--- scripts/hudson/initializeJBoss.bat
+++ scripts/hudson/initializeJBoss.bat
@@ -1,38 +1,21 @@
-rem CREATE A FILE TO DOWNLOAD JBOSS AND ORACLE DRIVER
-
-rem DOWNLOAD THOSE DEPENDENCIES
 cd %WORKSPACE%
-call ant -f scripts/hudson/download.xml -Dbasedir=. download
+call ant -f scripts/hudson/initializeDependencies.xml -Dbasedir=. initializeDatabase initializeJBoss -debug
 IF %ERRORLEVEL% NEQ 0 exit -1
 
-rem INITIALIZE HORNETQ
-cd %WORKSPACE%\hornetq-2.1.2.Final\config\jboss-as-5\
-set JBOSS_HOME=%WORKSPACE%\jboss-5.1.0.GA
-call build.bat
-set JBOSS_HOME=
-cd %WORKSPACE%
-
-rem INITIALIZE JBOSS
-cd %WORKSPACE%\jboss-5.1.0.GA\docs\examples\transactions
-call ant jts -Dtarget.server.dir=../../../server/all-with-hornetq
+cd %WORKSPACE%\jboss-as-7.1.0.Final\bin\
+call add-user admin password
+IF %ERRORLEVEL% NEQ 0 exit -1
+call add-user guest password -a
 IF %ERRORLEVEL% NEQ 0 exit -1
 cd %WORKSPACE%
-call ant replaceJBoss -DJBOSSAS_IP_ADDR=%JBOSSAS_IP_ADDR%
-IF %ERRORLEVEL% NEQ 0 exit -1
-
-rem INITIALZE BLACKTIE JBOSS DEPENDENCIES
-copy %WORKSPACE%\jatmibroker-xatmi\src\test\resources\hornetq-jms.xml %WORKSPACE%\jboss-5.1.0.GA\server\all-with-hornetq\conf
-cd %WORKSPACE%
-call ant configureHornetQ
-IF %ERRORLEVEL% NEQ 0 exit -1
 
 rem INITIALZE JBOSSESB
-copy %WORKSPACE%\scripts\hudson\hornetq\jboss-as-hornetq-int.jar %WORKSPACE%\jboss-5.1.0.GA\common\lib
-copy %WORKSPACE%\scripts\hudson\hornetq\hornetq-deployers-jboss-beans.xml %WORKSPACE%\jboss-5.1.0.GA\server\all-with-hornetq\deployers
-copy %WORKSPACE%\jbossesb-4.9\install\deployment.properties-example %WORKSPACE%\jbossesb-4.9\install\deployment.properties
-call ant configureESB -DWORKSPACE=%WORKSPACE:\=/%
-IF %ERRORLEVEL% NEQ 0 exit -1
-cd %WORKSPACE%\jbossesb-4.9\install
-call ant deploy
-IF %ERRORLEVEL% NEQ 0 exit -1
-cd %WORKSPACE%
+rem copy %WORKSPACE%\scripts\hudson\hornetq\jboss-as-hornetq-int.jar %WORKSPACE%\jboss-5.1.0.GA\common\lib
+rem copy %WORKSPACE%\scripts\hudson\hornetq\hornetq-deployers-jboss-beans.xml %WORKSPACE%\jboss-5.1.0.GA\server\all-with-hornetq\deployers
+rem copy %WORKSPACE%\jbossesb-4.9\install\deployment.properties-example %WORKSPACE%\jbossesb-4.9\install\deployment.properties
+rem call ant configureESB -DWORKSPACE=%WORKSPACE:\=/%
+rem IF %ERRORLEVEL% NEQ 0 exit -1
+rem cd %WORKSPACE%\jbossesb-4.9\install
+rem call ant deploy
+rem IF %ERRORLEVEL% NEQ 0 exit -1
+rem cd %WORKSPACE%
--- scripts/hudson/initializeJBoss.sh
+++ scripts/hudson/initializeJBoss.sh
@@ -3,21 +3,6 @@ if [ -z "${WORKSPACE}" ]; then
   exit -1;
 fi
 
-# GET JBOSS AND INITIALIZE IT
-if [ -d $WORKSPACE/jboss-as-7.1.0.Final ]; then
-  rm -rf $WORKSPACE/jboss-as-7.1.0.Final
-fi
-#if [ -d $WORKSPACE/jbossesb-4.9 ]; then
-#  rm -rf $WORKSPACE/jbossesb-4.9
-#fi
-
-cd $WORKSPACE
-if [ -e $WORKSPACE/jboss-as-7.1.0.Final.zip ]; then
-	echo "JBoss already downloaded"
-else
-	wget http://albany/userContent/jboss-as-7.1.0.Final.zip
-fi
-unzip jboss-as-7.1.0.Final.zip
 #if [ -e $WORKSPACE/jbossesb-4.9.zip ]; then
 #	echo "JBossESB already downloaded"
 #else
@@ -27,105 +12,10 @@ unzip jboss-as-7.1.0.Final.zip
 #' | unzip jbossesb-4.9.zip
 #
 
-echo 'JAVA_OPTS="-Xrunjdwp:transport=dt_socket,address=8787,server=y,suspend=n ${JAVA_OPTS}"' >> $WORKSPACE/jboss-as-7.1.0.Final/bin/standalone.conf
+ant -f scripts/hudson/initializeDependencies.xml -Dbasedir=. initializeJBoss -debug
 
-# INSTALL TRANSACTIONS
-# com.arjuna.orbportability.OrbPortabilityEnvironmentBean.bindMechanism
-echo 'JAVA_OPTS="$JAVA_OPTS -DOrbPortabilityEnvironmentBean.resolveService=NAME_SERVICE"' >> $WORKSPACE/jboss-as-7.1.0.Final/bin/standalone.conf
-sed -i 's?"spec"?"on"?g' $WORKSPACE/jboss-as-7.1.0.Final/standalone/configuration/standalone-full.xml
-sed -i 's?<coordinator-environment default-timeout="300"/>?<coordinator-environment default-timeout="300"/>\
-	    <jts/>?g' $WORKSPACE/jboss-as-7.1.0.Final/standalone/configuration/standalone-full.xml
-sed -i 's?inet-address value="127.0.0.1"?inet-address value="${jboss.bind.address:127.0.0.1}"?'g $WORKSPACE/jboss-as-7.1.0.Final/standalone/configuration/standalone-full.xml
-
-sed -n '1h;1!H;${;g;s?<logger category="com.arjuna">\n                <level name="WARN"/>?<logger category="com.arjuna">\n                <level name="ALL"/>?g;p;}' $WORKSPACE/jboss-as-7.1.0.Final/standalone/configuration/standalone-full.xml > .standalone-full.xml.bak
-mv .standalone-full.xml.bak $WORKSPACE/jboss-as-7.1.0.Final/standalone/configuration/standalone-full.xml
-
-#sed -i 's?            <root-logger>?            <logger category="org.hornetq">\
-#                <level name="WARN"/>\
-#            </logger>\
-#           <logger category="org.jboss.as.messaging">\
-#                <level name="WARN"/>\
-#            </logger>\
-#            <root-logger>?g' $WORKSPACE/jboss-as-7.1.0.Final/standalone/configuration/standalone-full.xml
-
-sed -i 's?            <root-logger>?            <logger category="org.jboss.narayana.blacktie">\
-                <level name="ALL"/>\
-            </logger>\
-            <logger category="org.codehaus.stomp">\
-                <level name="ALL"/>\
-            </logger>\
-            <logger category="org.hornetq">\
-                <level name="ALL"/>\
-            </logger>\
-            <root-logger>?g' $WORKSPACE/jboss-as-7.1.0.Final/standalone/configuration/standalone-full.xml
-
-sed -i 's?                </jms-destinations>?                    <jms-queue name="BTR_TestOne">\
-                        <entry name="queue/BTR_TestOne"/>\
-                        <entry name="java:jboss/exported/jms/queue/BTR_TestOne"/>\
-                    </jms-queue>\
-                    <jms-queue name="BTR_TestTwo">\
-                        <entry name="queue/BTR_TestTwo"/>\
-                        <entry name="java:jboss/exported/jms/queue/BTR_TestTwo"/>\
-                    </jms-queue>\
-                    <jms-queue name="BTC_ConvService">\
-                        <entry name="queue/BTC_ConvService"/>\
-                        <entry name="java:jboss/exported/jms/queue/BTC_ConvService"/>\
-                    </jms-queue>\
-                    <jms-queue name="BTR_JAVA_Converse">\
-                        <entry name="queue/BTR_JAVA_Converse"/>\
-                        <entry name="java:jboss/exported/jms/queue/BTR_JAVA_Converse"/>\
-                    </jms-queue>\
-                    <jms-topic name="BTR_JAVA_Topic">\
-                        <entry name="topic/BTR_JAVA_Topic"/>\
-                        <entry name="java:jboss/exported/jms/topic/BTR_JAVA_Topic"/>\
-                    </jms-topic>\
-                </jms-destinations>?g' $WORKSPACE/jboss-as-7.1.0.Final/standalone/configuration/standalone-full.xml
-
-
-# CONFIGURE SECURITY FOR THE ADMIN SERVICES
-sed -i 's?<security-settings>?<security-settings>\
-      <security-setting match="jms.queue.BTR_BTDomainAdmin">\
-         <permission type="send" roles="blacktie"/>\
-         <permission type="consume" roles="blacktie"/>\
-      </security-setting>\
-      <security-setting match="jms.queue.BTR_BTStompAdmin">\
-         <permission type="send" roles="blacktie"/>\
-         <permission type="consume" roles="blacktie"/>\
-      </security-setting>?g' $WORKSPACE/jboss-as-7.1.0.Final/standalone/configuration/standalone-full.xml
-
-# CONFIGURE HORNETQ TO NOT USE CONNECTION BUFFERING and TO NOT TIMEOUT INVM CONNECTIONS
-sed -i 's?<connection-factory name="InVMConnectionFactory">?<connection-factory name="InVMConnectionFactory">\
-      <consumer-window-size>0</consumer-window-size>\
-      <connection-ttl>-1</connection-ttl>\
-      <client-failure-check-period>86400000</client-failure-check-period>?g' $WORKSPACE/jboss-as-7.1.0.Final/standalone/configuration/standalone-full.xml
-
-#sed -i 's?<resourceadapter-class>org.hornetq.ra.HornetQResourceAdapter</resourceadapter-class>?<resourceadapter-class>org.hornetq.ra.HornetQResourceAdapter</resourceadapter-class>?
-#      <config-property>?
-#        <description>The connection TTL</description>?
-#        <config-property-name>ConnectionTTL</config-property-name>?
-#        <config-property-type>java.lang.Long</config-property-type>?
-#        <config-property-value>-1</config-property-value>?
-#      </config-property>?
-#      <config-property>?
-#        <description>The client failure check period</description>?
-#        <config-property-name>ClientFailureCheckPeriod</config-property-name>?
-#        <config-property-type>java.lang.Long</config-property-type>?
-#        <config-property-value>86400000</config-property-value>?
-#      </config-property>?g' $WORKSPACE/jboss-5.1.0.GA/server/all-with-hornetq/deploy/hornetq-ra.rar/META-INF/ra.xml
-
-#INSTALL JBossESB
-#cp $WORKSPACE/scripts/hudson/hornetq/jboss-as-hornetq-int.jar $WORKSPACE/jboss-5.1.0.GA/common/lib
-#cp $WORKSPACE/scripts/hudson/hornetq/hornetq-deployers-jboss-beans.xml $WORKSPACE/jboss-5.1.0.GA/server/all-with-hornetq/deployers
-#cd $WORKSPACE/jbossesb-4.9/install
-#cp deployment.properties-example deployment.properties
-#sed -i "s?/jbossesb-server-4.5.GA?$WORKSPACE/jboss-5.1.0.GA?" deployment.properties
-#sed -i "s?=default?=all-with-hornetq?" deployment.properties
-#sed -i "s?^org.jboss.esb.tomcat.home?#&?" deployment.properties
-#sed -i 's?/hornetq?&.sar?' build.xml
-#ant deploy
-
+chmod u+x $WORKSPACE/jboss-as-7.1.0.Final/bin/standalone.sh
+chmod u+x $WORKSPACE/jboss-as-7.1.0.Final/bin/add-user.sh
 
 (cd $WORKSPACE/jboss-as-7.1.0.Final/bin/ && JBOSS_HOME= ./add-user.sh admin password)
 (cd $WORKSPACE/jboss-as-7.1.0.Final/bin/ && JBOSS_HOME= ./add-user.sh guest password -a)
-
-sed -i 's?#guest=guest?guest=guest,blacktie?g' $WORKSPACE/jboss-as-7.1.0.Final/standalone/configuration/application-roles.properties
